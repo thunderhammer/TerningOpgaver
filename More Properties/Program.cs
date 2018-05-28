28204693d9b283b028f5259f8a381a4f208cea13
Morten Engemand <engemand@hotmail.com>
2018-05-28 11:35:28 +0200

    Commiting again

---
+++ More Properties/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Properties
+{
+    class Person
+    {
+        public string Fornavn { get; set; }
+
+        private string efternavn;
+        public string Efternavn
+        {
+            get
+            { return efternavn;
+            }
+
+            set
+            { if (value.Length > 3)
+                    this.efternavn = "";
+                else efternavn = value;
+            }
+        }
+
+     public string FuldtNavn()
+        {
+
+            return Fornavn + " " + Efternavn;
+        } 
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1= new Person();
+            p1.Efternavn = "Hansen";
+            p1.Fornavn = "Jens";
+            Console.WriteLine(p1.FuldtNavn());
+        }
+    }
+}
