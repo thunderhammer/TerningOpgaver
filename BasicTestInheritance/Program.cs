e1df0fcad33566ee6c69a7253e1d3ac5f03e1977
Morten Engemand <engemand@hotmail.com>
2018-05-29 13:36:27 +0200

    1

---
+++ BasicTestInheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTestInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.MetodeA();
+            B b = new B();
+            A aa = new C();
+            b.MetodeA();
+            b.MetodeB();
+            B bb = (B)aa; // Typecasting to access the methods of B
+
+            
+
+            
+            
+            
+
+
+        }
+        class A
+        {
+            public void MetodeA() { }
+        }
+        class B : A
+        {
+            public void MetodeB() { }
+        }
+
+        class C : A { }
+
+        class D { }
+    }
+}
