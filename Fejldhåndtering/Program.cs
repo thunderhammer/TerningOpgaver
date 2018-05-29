3fb104c239bad76084e287e95ec2a5375a211afc
Morten Engemand <engemand@hotmail.com>
2018-05-29 15:44:15 +0200

    Fejlhåndtering

---
+++ Fejldhåndtering/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejldhåndtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string s = null;
+                s.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Watchtower");
+            }
+
+
+
+        }
+    }
+}
---
+++ SimpelFejlhåndtering/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpelFejlhåndtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Der er sket en fejl. " + ex.Message);
+                throw;
+            }
+        }
+
+        private static void Run()
+        {
+
+            Console.WriteLine("Indtast 1. tal ");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast 2. tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            int res = tal1 + tal2;
+            Console.WriteLine("resultatet er " + res);
+
+        }
+
+
+    }
+}
