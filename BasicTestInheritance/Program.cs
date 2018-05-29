c7673dae810ff066b4646f9523f2523197b90ff1
Morten Engemand <engemand@hotmail.com>
2018-05-29 15:18:27 +0200

    Polyhalvfærdig

--- BasicTestInheritance/Program.cs
+++ BasicTestInheritance/Program.cs
@@ -16,16 +16,18 @@ namespace BasicTestInheritance
             A aa = new C();
             b.MetodeA();
             b.MetodeB();
-            B bb = (B)aa; // Typecasting to access the methods of B
+            //B bb = (B)aa; // Typecasting to access the methods of B
 
             A[] array = new A[3];
             array[0] = new A();
             array[1] = new B();
             array[2] = new C();
+            Hund h = new Hund();
+            //Console.WriteLine(h.ToString);
             foreach (var item in array)
             {
-                item.
-            }
+               
+            } 
             
         }
         class A
@@ -40,5 +42,10 @@ namespace BasicTestInheritance
         class C : A { }
 
         class D { }
+
+        class Hund
+        {
+            public string Navn { get; set; }
+        }
     }
 }
---
+++ Polymorfi med dyr/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorfi_med_dyr
+{
+    abstract class Dyr
+    {
+        public string Navn { get; set; }
+        static string sti = @"X:\dyrenavne.txt";
+        
+
+        public virtual void SigNoget()
+        {
+            Console.WriteLine("Jeg er et dyr og hedder ...");
+        }
+
+        static public Dyr TilfældigtDyr()
+        {
+            int HundEllerKat;
+            string[] navne = System.IO.File.ReadAllLines(sti);
+            Random R = new System.Random();
+            HundEllerKat = R.Next(0, 2);
+
+
+
+            if (HundEllerKat == 0)
+            {
+                return new Hund() { };
+            }
+
+            else
+            {
+                return new Kat() { };
+            }
+
+        }
+
+
+        class Hund : Dyr
+        {
+            public override void SigNoget()
+            {
+                Console.WriteLine("Jeg er en hund og hedder ...");
+            }
+        }
+
+        class Kat : Dyr
+        {
+            public override void SigNoget()
+            {
+                Console.WriteLine("Jeg er en kat og hedder ...");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Hund h1 = new Hund();
+                Hund h2 = new Hund();
+                Kat k1 = new Kat();
+                Kat k2 = new Kat();
+
+                h1.SigNoget();
+                k1.SigNoget();
+                h2.SigNoget();
+                k2.SigNoget();
+
+            }
+        }
+    }
+}
