101f1c4fd8c3d5f374ce11b1a08d4153d6ea7a78
Morten Engemand <engemand@hotmail.com>
2018-06-11 10:52:04 +0200

    More interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Kunde k = new Kunde();
+            k.Gem();
+
+            Vare v = new Vare();
+            v.Gem();
+        }
+    }
+
+    interface IDbfunktioner
+    {
+        void Gem();
+        void Reset();
+
+    }
+
+    class Kunde : IDbfunktioner
+    {
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer kunder");
+
+        }
+        public void Reset()
+        {
+
+        }
+    }
+
+    class Vare : IDbfunktioner
+    {
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer varer");
+        }
+        public void Reset()
+        {
+
+        }
+    }
+}
