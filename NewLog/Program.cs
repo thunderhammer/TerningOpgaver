fd80ec826ce46df81ea0f4d399f08b1241d26832
Morten Engemand <engemand@hotmail.com>
2018-06-11 10:09:38 +0200

    NoLog

---
+++ NewLog/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewLog
+{
+    class Program
+    {
+        static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void F1(int a)
+        {
+
+        }
+
+    }
+}
